2ef113c93725fccebfc1c183ed3a550fd09ed1d0
Daniel Navarrete Cuevas <danielnavarrete2304@gmail.com>
2025-02-18 19:48:26 -0800

    Modified landing page and about

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,33 +8,33 @@ import highway from '../assets/images/highway.png';
 
 
 const About = () => {
-  return <div>
+    return <div>
   
-            <div className="landing-audi1">
-                <img src={etron} alt="The landing page car" className="landing-img" />
-                <h1>You have to live it To believe it</h1>
-            </div>
-            <div className="who-are">
-                <h1>Our roots define us</h1>
-                <div className="this-is-us">
-                    <img src={engine} alt="audi-engine" />
-                    <img src={interior} alt="audi-interiors" />
-                    
-                    
-                </div>
-                <p className="the-text">"We believe that any owner of our cars should be the happiest person in the world, while maintaining class, fancy and a comfy environment."</p>
-            </div>
-            <div className="mission">
-                <img src={about1} alt="The etron is here"/>
-                <h1 className="the-mission">Our Mission</h1>
-                <p className="mission-description">"To lead the automotive industry by providing innovative electric and combustion engine vehicles that combine performance, sustainability, and advanced technology, driving a cleaner, more efficient future."</p>
-            </div>
-            <div className="vision">
-                <img src={highway} alt="The etron is here"/>
-                <h1 className="the-vission">Our Mission</h1>
-                <p className="vission-description">"Inspiring every journey while driving toward a sustainable future."</p>
-            </div>
+    <div className="landing-audi">
+        <img src={etron} alt="The landing page car" className="landing-img" />
+        <h1>You have to live it To believe it</h1>
+    </div>
+    <div className="who-are">
+        <h1>Our roots define us</h1>
+        <div className="this-is-us">
+            <img src={engine} alt="audi-engine" />
+            <img src={interior} alt="audi-interiors" />
+            
+            
         </div>
+        <p className="the-text">"We believe that any owner of our cars should be the happiest person in the world, while maintaining class, fancy and a comfy environment."</p>
+    </div>
+    <div className="mission">
+        <img src={about1} alt="The etron is here"/>
+        <h1 className="the-mission">Our Mission</h1>
+        <p className="mission-description">"To lead the automotive industry by providing innovative electric and combustion engine vehicles that combine performance, sustainability, and advanced technology, driving a cleaner, more efficient future."</p>
+    </div>
+    <div className="vision">
+        <img src={highway} alt="The etron is here"/>
+        <h1 className="the-vission">Our Mission</h1>
+        <p className="vission-description">"Inspiring every journey while driving toward a sustainable future."</p>
+    </div>
+</div>
 };
 
 export default About; 
